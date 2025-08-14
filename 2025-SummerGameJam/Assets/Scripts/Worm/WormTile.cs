cbd60a29c6ea7a677943d287ed8f46b9bae3bf1e
Kim Dasan <67263093+Plagem@users.noreply.github.com>
2025-08-14 23:31:07 +0900

    feat : Object 드래그 드롭 0.2

--- 2025-SummerGameJam/Assets/Scripts/Worm/WormTile.cs
+++ 2025-SummerGameJam/Assets/Scripts/Worm/WormTile.cs
@@ -1,9 +1,12 @@
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class WormTile : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class WormTile : MonoBehaviour
 {
-    private float Rad = 1f;
+    private float hexSize = 1f;
+    
+    public GameObject WormPrefab;
     
     public void Initialize(WormInfo wormInfo)
     {
@@ -11,24 +14,47 @@ public class WormTile : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDrag
 
         for (int i = 0; i < wormInfo.WormPosList.Count; i++)
         {
-            
+            var curPos = wormInfo.WormPosList[i];
+            float spawnX = curPos.x % 2 == 0 ? curPos.y * Mathf.Sqrt(3) / 2f * hexSize : (curPos.y + 0.5f) * Mathf.Sqrt(3) / 2f * hexSize;
+            float spawnY = curPos.x * 0.75f * hexSize;
+
+            Vector2 spawnPos = new Vector2(spawnX, spawnY);
+
+            Instantiate(WormPrefab, spawnPos, quaternion.identity, transform);
         }
         // posList에 맞게 타일을 그려준다
         
     }
-    
-    public void OnBeginDrag(PointerEventData eventData)
+
+    private Vector2 originPos;
+    private Vector3 offset;
+    private float zCoord;
+
+    void OnMouseDown()
     {
-        throw new System.NotImplementedException();
+        originPos = transform.position;
+        
+        // 클릭 시 오브젝트까지의 Z좌표 저장
+        zCoord = Camera.main.WorldToScreenPoint(transform.position).z;
+        // 클릭한 지점과 오브젝트 중심의 거리 계산
+        offset = transform.position - GetMouseWorldPos();
     }
 
-    public void OnEndDrag(PointerEventData eventData)
+    void OnMouseDrag()
     {
-        throw new System.NotImplementedException();
+        // 마우스 위치 + offset 만큼 이동
+        transform.position = GetMouseWorldPos() + offset;
     }
 
-    public void OnDrag(PointerEventData eventData)
+    void OnMouseUp()
     {
-        throw new System.NotImplementedException();
+        transform.position = originPos;
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = zCoord; // Z 고정
+        return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 }
